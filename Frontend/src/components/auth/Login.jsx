bfe8deca1f633eaff76c870afedc7f25b6da7023
Sujit kumar Shaw <161234730+sujit-27@users.noreply.github.com>
2026-02-09 12:07:11 +0530

    Update Login.jsx

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -55,7 +55,7 @@ export default function Login() {
   const loginWithGoogle = useGoogleLogin({
     flow: "implicit",            // or "auth-code"
     ux_mode: "redirect",         // 👈 THIS FIXES COOP
-    redirect_uri: "http://localhost:5173/",
+    redirect_uri: "https://mail-forge-plum.vercel.app/",
     onSuccess: async (tokenResponse) => {
       setLoading(true);
       try {
